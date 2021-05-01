1798727bf5f72d5bf96993f5cb249d7f250a07f5
Sheng Yang <yangsheng@njmu.edu.cn>
2021-05-01 15:51:04 +0800

    Update DBSLMM_script.sh

--- DBSLMM_script.sh
+++ DBSLMM_script.sh
@@ -35,7 +35,6 @@ while getopts "D:p:B:s:m:H:n:G:P:l:T:c:i:t:o:" opt; do
   esac
 done
 
-
 printf "\033[33mArgument software_path is %s  \033[0m\n" "$software_path"
 printf "\033[33mArgument plink is %s  \033[0m\n" "$plink"
 printf "\033[33mArgument block_prefix is %s  \033[0m\n" "$block_prefix"
@@ -46,23 +45,19 @@ printf "\033[33mArgument val_geno_prefix is %s  \033[0m\n" "$val_geno_prefix"
 printf "\033[33mArgument type is %s  \033[0m\n" "$type"
 if [[ "${type}" == "t" ]]
 then
-printf "\033[33mArgument valid_pheno is %s  \033[0m\n" "$val_pheno"
-printf "\033[33mArgument col is %s  \033[0m\n" "$col"
-printf "\033[33mArgument index is %s  \033[0m\n" "$index"
+	printf "\033[33mArgument valid_pheno is %s  \033[0m\n" "$val_pheno"
+	printf "\033[33mArgument col is %s  \033[0m\n" "$col"
+	printf "\033[33mArgument index is %s  \033[0m\n" "$index"
 fi
 if [ -n "$cov" ]; then 
-printf "\033[33mArgument cov is %s  \033[0m\n" "$cov"
+	printf "\033[33mArgument cov is %s  \033[0m\n" "$cov"
 fi
 printf "\033[33mArgument thread is %s  \033[0m\n" "$thread"
 printf "\033[33mArgument outpath is %s  \033[0m\n" "$outpath"
 
-# DBSLMM software
-DBSLMM=${software_path}DBSLMM/software/DBSLMM.R
+DBSLMM=${software_path}DBSLMM/software/DBSLMM1.R
 TUNE=${software_path}DBSLMM/software/TUNE.R
-dbslmm=${software_path}/DBSLMM/software/dbslmm
-
-# split to chromosome
-Rscript ${SPLIT} --summary ${summary_file_prefix}.assoc.txt
+dbslmm=${software_path}/DBSLMM/scr/dbslmm
 
 # LDSC: heritability and number of SNP
 nsnp=`sed -n '24p' ${herit} | cut -d ',' -f 2 | cut -d ' ' -f 2`
@@ -71,49 +66,49 @@ h2=`sed -n '26p' ${herit} | cut -d ":" -f 2 | cut -d '(' -f 1 | cut -d " " -f 2`
 # DBSLMM: tuning version
 if [[ "$type" == "t" ]]
 then
-for chr in `seq 1 22`
-do
+	for chr in `seq 1 22`
+	do
 
-BLOCK=${block_prefix}${chr}
-summchr=${summary_file_prefix}_chr${chr}
-nobs=`sed -n "2p" ${summchr}.assoc.txt.gz | awk '{print $5}'`
-nmis=`sed -n "2p" ${summchr}.assoc.txt.gz | awk '{print $4}'`
-n=$(echo "${nobs}+${nmis}" | bc -l)
-Rscript ${DBSLMM} --summary ${summchr}.assoc.txt --outPath ${outpath} --plink ${plink}\
-                  --dbslmm ${dbslmm} --ref ${val_geno} --n ${n} --type ${type} --nsnp ${nsnp} --block ${BLOCK}.bed\
-                  --h2 ${herit} --h2f 0.8,1,1.2 --thread ${thread}
-for h2f in 0.8 1 1.2
-do
-if [ -f "${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.badsnps" ];then
-rm ${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.badsnps
-fi
-summchr_prefix=`echo ${summchr##*/}`
-${plink}  --silent --bfile ${val_geno} --score ${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.txt 1 2 4 sum\
-          --out ${outpath}${summchr_prefix}_h2f${h2f}
-rm ${outpath}${summchr_prefix}_h2f${h2f}.log
-if [ -f "${outpath}${summchr_prefix}_h2f${h2f}.nopred" ];then
-rm ${outpath}${summchr_prefix}_h2f${h2f}.nopred
-fi
+		BLOCK=${block_prefix}${chr}
+		summchr=${summary_file_prefix}${chr}
+		nobs=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $5}'`
+		nmis=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $4}'`
+		n=$(echo "${nobs}+${nmis}" | bc -l)
+		val_geno=${val_geno_prefix}${chr}
+		Rscript ${DBSLMM} --summary ${summchr}.assoc.txt --outPath ${outpath} --plink ${plink} --model ${model}\
+						  --dbslmm ${dbslmm} --ref ${val_geno} --n ${n} --type ${type} --nsnp ${nsnp} --block ${BLOCK}.bed\
+						  --h2 ${h2} --h2f 0.8,1,1.2 --thread ${thread}
+		for h2f in 0.8 1 1.2
+		do
+			if [ -f "${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.badsnps" ];then
+				rm ${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.badsnps
+			fi
+			summchr_prefix=`echo ${summchr##*/}`
+			${plink}  --silent --bfile ${val_geno} --score ${outpath}${summchr_prefix}_h2f${h2f}.dbslmm.txt 1 2 4 sum\
+					  --out ${outpath}${summchr_prefix}_h2f${h2f}
+			rm ${outpath}${summchr_prefix}_h2f${h2f}.log
+			if [ -f "${outpath}${summchr_prefix}_h2f${h2f}.nopred" ];then
+				rm ${outpath}${summchr_prefix}_h2f${h2f}.nopred
+			fi
+		done
+	done
 
-done
-done
+	summchr_prefix2=`echo ${summchr_prefix%_*}`
+	if [[ ! -n "$cov" ]]
+	then 
+	Rscript ${TUNE} --phenoPred ${outpath}${summchr_prefix2} --phenoVal ${val_pheno},${col} \
+		   --h2Range 0.8,1,1.2 --index ${index}
+	else 
+	Rscript ${TUNE} --phenoPred ${outpath}${summchr_prefix2} --phenoVal ${val_pheno},${col} \
+		   --h2Range 0.8,1,1.2 --index ${index} --cov ${cov}
+	fi
 
-summchr_prefix2=`echo ${summchr_prefix%_*}`
-if [[ "${cov}" == "N" ]]
-then 
-Rscript ${TUNE} --phenoPred ${outpath}${summchr_prefix2} --phenoVal ${val_pheno},${col} \
-       --h2Range 0.8,1,1.2 --index ${index}
-else 
-Rscript ${TUNE} --phenoPred ${outpath}${summchr_prefix2} --phenoVal ${val_pheno},${col} \
-       --h2Range 0.8,1,1.2 --index ${index} --cov ${cov}
-fi
-
-hbest=`cat ${outpath}${summchr_prefix2}_hbest.${index}`
-for chr in `seq 1 22`
-do
-mv ${outpath}${summchr_prefix2}_chr${chr}_h2f${hbest}.dbslmm.txt ${outpath}${summchr_prefix2}_chr${chr}_best.dbslmm.txt
-rm ${outpath}${summchr_prefix2}_chr${chr}_h2f*
-done
+	hbest=`cat ${outpath}${summchr_prefix2}_hbest.${index}`
+	for chr in `seq 1 22`
+	do
+		mv ${outpath}${summchr_prefix2}_chr${chr}_h2f${hbest}.dbslmm.txt ${outpath}${summchr_prefix2}_chr${chr}_best.dbslmm.txt
+		rm ${outpath}${summchr_prefix2}_chr${chr}_h2f*
+	done
 
 fi
 
@@ -122,17 +117,19 @@ if [[ "$type" == "d" ]]
 then
 for chr in `seq 1 22` 
 do
-BLOCK=${block_prefix}${chr}
-summchr=${summary_file_prefix}${chr}
-val_geno=${val_geno}${chr}
-nobs=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $5}'`
-nmis=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $4}'`
-n=$(echo "${nobs}+${nmis}" | bc -l)
-Rscript ${DBSLMM} --summary ${summchr}.assoc.txt --outPath ${outpath} --plink ${plink}\
-                  --dbslmm ${dbslmm} --ref ${val_geno} --n ${n} --nsnp ${nsnp} --block ${BLOCK}.bed\
-                  --h2 ${herit} --thread ${thread}
-summchr_prefix=`echo ${summchr##*/}`
-rm ${outpath}${summchr_prefix}.dbslmm.badsnps
+	BLOCK=${block_prefix}${chr}
+	summchr=${summary_file_prefix}${chr}
+	val_geno=${val_geno_prefix}${chr}
+	summchr=${summary_file_prefix}${chr}
+	nobs=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $5}'`
+	nmis=`sed -n "2p" ${summchr}.assoc.txt | awk '{print $4}'`
+	n=$(echo "${nobs}+${nmis}" | bc -l)
+	echo ${model}
+	Rscript ${DBSLMM} --summary ${summchr}.assoc.txt --outPath ${outpath} --plink ${plink} --model ${model}\
+					  --dbslmm ${dbslmm} --ref ${val_geno} --n ${n} --nsnp ${nsnp} --block ${BLOCK}.bed\
+					  --h2 ${h2} --thread ${thread}
+	summchr_prefix=`echo ${summchr##*/}`
+	rm ${outpath}${summchr_prefix}.dbslmm.badsnps
 
 done
 fi
